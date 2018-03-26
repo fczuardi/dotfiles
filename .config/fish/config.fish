9b7b9ab36a3e19c867709a298ce4473c56381415
Fabricio C Zuardi <fabricio@fabricio.org>
2018-03-26 10:32:47 -0300

    fix node not opening on fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ fish_vi_key_bindings
 fundle plugin 'edc/bass'
 fundle init
 
-function nvm 
-  bass source ~/.nvm/nvm.sh --no-use ';' nvm use node
+function nvm
+  bass source ~/.nvm/nvm.sh --no-use ';' nvm use --delete-prefix v9.9.0
 end
 

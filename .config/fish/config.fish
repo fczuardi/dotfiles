aa979f89dc8d90f450cf573b4216ce8950f7fa7c
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-03 00:43:54 -0300

    add github shortcut to add ssh key to agent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,12 @@
+# Vim key bindings
 fish_vi_key_bindings
 
-fundle plugin 'edc/bass'
-fundle init
-
-function nvm
-  bass source ~/.nvm/nvm.sh --no-use ';' nvm use --delete-prefix v9.10.0
-end
-
+# thefuck
 env SHELL=/usr/bin/fish thefuck --alias | source
 
-# use PATH from bash
-bass source ~/.profile
+# github ssh key
+function github
+  eval (ssh-agent -c)
+  ssh-add
+end
 

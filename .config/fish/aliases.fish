16ba4fe3dc802d69adaa259327dd5cd917bdbfd5
Fabricio C Zuardi <fabricio@fabricio.org>
2018-11-18 15:22:25 -0200

    new i3 config tweaks

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,2 +1,10 @@
 alias gs 'git status'
 alias gd 'git difftool --no-symlinks --dir-diff'
+
+# stoq
+alias cdsp 'cd ~/stoq/stoq-link-premium'
+alias cdsa 'cd ~/stoq/stoq-link-admin'
+alias cdss 'cd ~/stoq/stoq'
+alias cdsw 'cd ~/stoq/stoq-web'
+alias cdsl 'cd ~/stoq/stoq-web/stoq-link'
+alias cdst 'cd ~/stoq/stoq-mobile-pos'

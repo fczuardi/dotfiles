86f2c41592c52fe348b5026d8da9c0c5ca5119dd
Fabricio C Zuardi <fabricio@fabricio.org>
2018-12-02 11:47:48 -0200

    update stoq aliases

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,10 +1,20 @@
+# git
 alias gs 'git status'
 alias gd 'git difftool --no-symlinks --dir-diff'
+alias gl 'git log --all --graph --abbrev-commit'
 
 # stoq
+alias cds 'cd ~/stoq'
+alias cdss 'cd ~/stoq/stoq'
+alias cdsl 'cd ~/stoq/stoq-web/stoq-link'
 alias cdsp 'cd ~/stoq/stoq-link-premium'
 alias cdsa 'cd ~/stoq/stoq-link-admin'
-alias cdss 'cd ~/stoq/stoq'
-alias cdsw 'cd ~/stoq/stoq-web'
-alias cdsl 'cd ~/stoq/stoq-web/stoq-link'
 alias cdst 'cd ~/stoq/stoq-mobile-pos'
+alias cdsss 'cd ~/stoq/stoq-server'
+
+# kitty
+alias icat 'kitty +kitten icat'
+
+#vim
+alias vi vim
+alias vimp 'vim -p'
